7948d77721bf76dfc2b5ad4d133a9dc6c07ad001
Richard Anton <ranton@ranton.org>
2022-12-12 12:11:13 -0800

    Initial Commit

---
+++ opengl_setup_example/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  opengl_setup_example
+//
+//  Created by Richard Anton on 12/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+// TODO: move to readme
+
+//- install Homebrew
+//- brew install glfw3
+//- brew instll glew
+// Create an xcode cli project, pick C++.
+// build setting: search for paths, header, click +, /usr/local/include
+// (or wherever they are, might be different on M1
+// for Apple Silicon (M1,M2) mac native homebrew, headers  are in /opt/homebrew/include instead.
+// add the libraries from /usr/local or wherever for glfw3 and glew
+
