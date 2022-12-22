28451cd5b7150e64b949c562e6427ce3e5264479
Richard Anton <ranton@ranton.org>
2022-12-22 08:10:51 -0800

    Improve fragment shader a bit

--- opengl_setup_example/phong_fragment_shader.glsl
+++ opengl_setup_example/phong_fragment_shader.glsl
@@ -1,6 +1,5 @@
 #version 330 core
 out vec3 color;
-// in vec4 v_color;
 
 in vec2 UV;
 
@@ -11,20 +10,27 @@ in vec3 esNormal;
 in vec3 halfVector;
 
 uniform sampler2D myTextureSampler;
+
+// diffuse color comes from the
+// uniform vec3 materialAmbient;
+// uniform vec3 materialSpecular
+// uniform float materialSpecExp;
+
 //uniform vec3 globalAmbient;
-//uniform vec3 materialAmbient;
 //uniform vec3 lightDiffuse;
+//uniform vec3 lightSpecular
 
 void main()
 {
-  //  color = texture( myTextureSampler, UV ).rgb;
     // TODO: use uniforms for these.
-    vec3 globalAmbient = vec3(0.1, 0.08, 0.11);
-    vec3 materialAmbient = vec3(1,1,1);
-    vec3 lightDiffuse = vec3(1,1,1);
-    vec3 lightSpecular = vec3(1,1,1);
+    vec3 materialAmbient = vec3(0.1, 0.08, 0.11);
+    vec3 materialDiffuse = vec3(0.8, 0.8, 0.8);
     vec3 materialSpecular = vec3(1,1,1);
     float materialSpecExp = 10;
+
+    vec3 globalAmbient = vec3(1,1,1);
+    vec3 lightDiffuse = vec3(1,1,1);
+    vec3 lightSpecular = vec3(1,1,1);
 
     vec3 L = normalize(esLightDirection);
     vec3 N = normalize(esNormal);
@@ -37,8 +43,8 @@ void main()
     vec3 texColor = texture( myTextureSampler, UV ).rgb;
     
     vec3 ambient = (globalAmbient * materialAmbient).xyz;
-    vec3 diffuse = lightDiffuse.xyz * texColor * max(cosTheta, 0.0);
+    vec3 diffuse = lightDiffuse.xyz * materialDiffuse * max(cosTheta, 0.0);
     vec3 specular = lightSpecular.xyz * materialSpecular.xyz * pow(max(cosPhi,0.0), materialSpecExp);
     
-    color = ambient + diffuse + specular;
+    color = (ambient + diffuse) * texColor + specular;
 }
